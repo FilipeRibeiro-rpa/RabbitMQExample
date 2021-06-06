55580ebe7e9c24799291d8543439b51bbbc8d39f
filipe <ribeiro12369@hotmail.com>
2021-06-06 15:01:58 -0300

    first commit

---
+++ src/Consumer.Message/Program.cs
@@ -0,0 +1,52 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer.Message
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create String of Connection Factory
+            var connectionFactory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            // Connect RabbitMq
+            using (var connection = connectionFactory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // tests queue query
+                channel.QueueDeclare(
+                    queue: "tests",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                
+                consumer.Received += (sender, eventArgs) =>
+                {
+                    var message = Encoding.UTF8.GetString(eventArgs.Body.ToArray());
+
+                    Console.WriteLine(Environment.NewLine + "[New message received] " + message);
+                };
+
+                channel.BasicConsume(queue: "tests",
+                     autoAck: true,
+                     consumer: consumer);
+
+                Console.WriteLine("Waiting messages to proccess");
+                Console.WriteLine("Press some key to exit...");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
